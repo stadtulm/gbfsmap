552d5935aacfcc5b1ad5a343c1f31e28b2306315
ubahnverleih <ubahnverleih@gmail.com>
2019-08-20 21:43:34 +0200

    zoom level

--- src/map.ts
+++ src/map.ts
@@ -8,7 +8,7 @@ declare var API_ROOT;
 export class Map {
 	constructor() {
 		console.log("Hello Map")
-		this.map = L.map("map").setView([53.03134, 13.30776], 15);
+		this.map = L.map("map").setView([53.03134, 13.30776], 16);
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
 			maxZoom: 19
