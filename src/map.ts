0387371bd970de0576ec6c662b3237130fd9c872
balu-dev <kathi.schweiger@posteo.de>
2019-09-25 17:46:15 +0200

    Map: switched to Wikimedia map tiles

--- src/map.ts
+++ src/map.ts
@@ -5,8 +5,8 @@ export class Map {
 	constructor() {
 		console.log("Hello Map")
 		this.map = L.map("map").setView([51.505, -0.09], 13);
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+		L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+			attribution: '<a href="https://foundation.wikimedia.org/wiki/Maps_Terms_of_Use">Wikimedia Maps</a> | Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
 			maxZoom: 19
 		}).addTo(this.map)
 
